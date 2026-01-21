6ed6460d1da3418b4a46d7803ad87dba86935a99
Dayvidsqn <dayvidsiesquen@gmail.com>
2026-01-21 16:09:31 -0500

    update

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL //|| `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  connectionString: process.env.DATABASE_URL, //|| `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   ssl: { rejectUnauthorized: false } // necesario para Render
 });
 
